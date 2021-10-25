a13122b1c631c4f1f89f794f7cf8d6ec511b36eb
socdoneleft <socdoneleft@gmail.com>
2021-10-25 10:40:09 -0500

    init

---
+++ README.md
@@ -0,0 +1,8 @@
+# hey buds!
+what I do:
+- make socialist & progressive YouTube vids: [https://youtu.be/x9VuTRM7RTI](https://youtu.be/x9VuTRM7RTI)
+- maintain the Stinky Rightwinger Factsheet (warning: large): [https://socdoneleft.github.io/srf](https://socdoneleft.github.io/srf)
+- maintain PolitiCurves, a data-based political ideology quiz: [http://politicurves.org/](http://politicurves.org/)
+- occasionally stream (games, debates): [https://www.twitch.tv/socdoneleft](https://www.twitch.tv/socdoneleft)
+- (planned) construct a "LeftTuber Praxis Metric" to measure YouTuber efficacy
+- (planned) construct an "Economic Leftism Index" a la the Economic Freedom Index
---
+++ budget.R
@@ -0,0 +1,42 @@
+#####
+# init
+#####
+# clear old data
+rm(list=ls(all=TRUE))
+
+# set working directory
+data_wd = "C:/Users/desk/google_drive/socdoneleft.github.io/"
+setwd(data_wd)
+
+# load packages
+require("data.table")
+require("ggplot2")
+require("readxl")
+
+#####
+# read data
+#####
+# read csv
+budget = read_excel("budget.xlsx")
+budget = data.table(budget)
+
+# reformat columns
+budget[, date := as.Date(date, format = "%Y-%m-%d UTC")]
+budget[, yearmonth := format(date, "%Y-%m")]
+
+# skip last row
+budget = budget[-dim(budget)[1], ]
+
+#####
+# graph data
+#####
+summary = budget[, .(sumearnings = sum(earnings)), by = c("yearmonth", "type")]
+summary = summary[-c(1:2), ]
+
+ggplot(summary, aes(x = yearmonth, y = sumearnings, fill = type, group = type)) +
+  geom_bar(stat = "identity")
+
+summary = summary[, .(cumsumearnings = cumsum(sumearnings), yearmonth = yearmonth), by = c("type")]
+
+ggplot(summary, aes(x = yearmonth, y = cumsumearnings, color = type, group = type)) +
+  geom_line(size = 2)
